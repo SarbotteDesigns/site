c297eebd4d7174ffbaeec0831eb9a9a7a6a4418d
Nicolas Barbotte <n.barbotte@gmail.com>
2012-12-21 12:02:38 +0100

    nettoyage

--- server.rb
+++ server.rb
@@ -15,63 +15,22 @@ end
 
 post '/application/?' do
   nom = params[:nom]
-  application =
-"""
-## Informations
-
-**Nom**
-
-> #{params[:prenom]} #{params[:nom]}
-
-{::comment}
-#{params[:email]}
-{:/comment}
-
-## Sarbottisme
-
-**Veuillez décrire de façon succinte et détaillée votre expérience du Sarbottisme**
-
-> #{params[:experienceSarbottisme]}
-
-**Comment avez-vous connu Sarbotte Designs ?**
-
-> #{params[:connuSarbotteDesigns]}
-
-## Test d'aptitudes
-
-Vos tests d'aptitudes sont actuellement analisés et vous seront communiqués utlérieurement par Sarbotte Designs.
-
-{::comment}
-
-**Que renvoie l'expression suivante :**
-
-> #{params[:testJs]}
-
-**Quels sont les cinq pilliers du Sarbottisme ?**
-
-> #{params[:pilliersSarbottisme]}
-
-{:/comment}
-
-"""
-  File.open("public/applications/#{nom}.md", 'w:UTF-8') {|f| f.write(application) }
+  application = File.open("views/application.md", "rb:UTF-8").read
+  File.open("public/applications/#{nom}.md", 'w:UTF-8') {|f| f.write(eval '"' + application + '"') }
   redirect to("/application/#{nom}")
 end
 
 get '/application/:nom/?' do |nom|
-  file = File.open("public/applications/#{nom}.md", "rb:UTF-8")
-  application = file.read
-
-  # application = File.read("public/applications/#{nom}.md")
+  application = File.open("public/applications/#{nom}.md", "rb:UTF-8").read
 
   application = Kramdown::Document.new(application).to_html
   haml :showApplication, :locals=>{:application => application}
 end
 
 not_found do
-  'This is nowhere to be found.'
+  'Page non trouvée.'
 end
 
 error do
-  'Sorry there was a nasty error - ' + env['sinatra.error'].name
+  'Erreur - ' + env['sinatra.error'].name
 end
---
+++ views/application.md
@@ -0,0 +1,35 @@
+## Informations
+
+**Nom**
+
+> #{params[:prenom]} #{params[:nom]}
+
+{::comment}
+#{params[:email]}
+{:/comment}
+
+## Sarbottisme
+
+**Veuillez décrire de façon succinte et détaillée votre expérience du Sarbottisme**
+
+> #{params[:experienceSarbottisme]}
+
+**Comment avez-vous connu Sarbotte Designs ?**
+
+> #{params[:connuSarbotteDesigns]}
+
+## Test d'aptitudes
+
+Vos tests d'aptitudes sont actuellement analisés et vous seront communiqués utlérieurement par Sarbotte Designs.
+
+{::comment}
+
+**Que renvoie l'expression suivante :**
+
+> #{params[:testJs]}
+
+**Quels sont les cinq pilliers du Sarbottisme ?**
+
+> #{params[:pilliersSarbottisme]}
+
+{:/comment}
