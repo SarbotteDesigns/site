01b0371842efc1dcd8d85905d624cece201a1253
Nicolas Barbotte <n.barbotte@gmail.com>
2013-02-05 21:14:26 +0100

    maj

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source :rubygems
 gem 'sinatra', '1.3.2'
 gem 'haml', '3.1.6'
 gem 'kramdown', '0.14.1'
+gem 'sqt', '1.1.0'
+gem 'json', '1.7.6'
--- public/scripts/sqt.js
+++ public/scripts/sqt.js
@@ -36,6 +36,12 @@
     editor.setTheme("ace/theme/clouds");
     editor.getSession().setMode("ace/mode/html");
     editor.renderer.setPadding(20);
+    editor.setValue('<html>\n    <head>\n        <title>Sarbotte Quality Test</title>\n        <script>\n            console.log("page init");\n        </script>\n    </head>\n    <body>\n        Sarbotte Quality Test\n    </body>\n</html>');
+    $('#total').html('208');
+    $('#jsAndCss').html('47');
+    $('#sqi').html('77.4');
+    editor.focus();
+    editor.selection.clearSelection()
     editor.getSession().on("change", debounce(function(){
         $.ajax({
           url: "/sqt",
