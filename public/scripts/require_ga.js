e7782f684fb32bb7c0ef1b6fcd1a00d17ae45bd7
Nicolas Barbotte <n.barbotte@gmail.com>
2013-02-15 11:15:15 +0100

    ga via require

---
+++ public/scripts/require_ga.js
@@ -0,0 +1,11 @@
+define(function(){
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-38362238-1']);
+  _gaq.push(['_setDomainName', 'sarbotte-designs.com']);
+  _gaq.push(['_trackPageview']);
+  window._gaq = _gaq;
+
+  ga = require(['http://www.google-analytics.com/ga.js'], function(){});
+
+});
